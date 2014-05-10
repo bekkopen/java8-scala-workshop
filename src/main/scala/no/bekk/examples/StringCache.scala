8f6acecb25cf701ccb7003481cf1692042494a96
Matias Vinjevoll <matiasvinjevoll@gmail.com>
2014-05-10 20:55:40 +0200

    Add slides for Streams, Default Methods and Traits with example.

---
+++ src/main/scala/no/bekk/examples/StringCache.scala
@@ -0,0 +1,24 @@
+package no.bekk.examples
+
+case class StrigCache() extends Retrievable[String] with Cache[String] {
+  def retrieve = ???
+}
+
+trait Retrievable[T] {
+  def retrieve: T
+}
+
+trait Cache[T] {
+  self: Retrievable[T] =>
+
+  var value: Option[T] = None
+
+  def getValue: T = value.getOrElse {
+    value = Some(retrieve)
+    value.get
+  }
+
+  def invalidate {
+    value = None
+  }
+}
