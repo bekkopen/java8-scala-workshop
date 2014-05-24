c18873302fc42e4e550294babf71a3104d028248
Matias Vinjevoll <matiasvinjevoll@gmail.com>
2014-05-24 22:13:01 +0200

    Implement excercise 4 with tests in Scala.

---
+++ src/main/scala/no/bekk/scala/excercises/Ex4_LambdaParallel.scala
@@ -0,0 +1,18 @@
+package no.bekk.scala.excercises
+
+import no.bekk.scala.model.Team
+
+object Ex4_LambdaParallel {
+
+	def teamsWithValueHigherThan(value: Double, teams: List[Team]) =
+		teams.par.filter(slowPredicate(_.value > value)).toList
+
+	private def slowPredicate[A](predicate: A => Boolean): A => Boolean = x => {
+		try {
+			Thread.sleep(100)
+		} catch {
+			case e: InterruptedException => e.printStackTrace
+		}
+		predicate(x)
+	}
+}
---
+++ src/test/scala/no/bekk/scala/excercises/Ex4_LambdaParallelSpec.scala
@@ -0,0 +1,24 @@
+package no.bekk.scala.excercises
+
+import org.scalatest.{Matchers, FlatSpec}
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import no.bekk.scala.model.Data._
+
+@RunWith(classOf[JUnitRunner])
+class Ex4_LambdaParallelSpec extends FlatSpec with Matchers {
+
+	"teamsWithValueHigherThan" should "return teams with value higher than a given value, filtered by a slow predicate" in {
+		val teams = manchesterUnited :: liverpool :: arsenal :: borussiaDortmund :: atleticoMadrid :: Nil
+
+		val before = System.currentTimeMillis
+		val teamsWithHigherValueThan1B = Ex4_LambdaParallel.teamsWithValueHigherThan(1000000000.0, teams)
+		val after = System.currentTimeMillis
+		val totalTime = after - before
+
+		totalTime should be > 100L
+		totalTime should be < 450L
+		teamsWithHigherValueThan1B should contain allOf(manchesterUnited, arsenal)
+		teamsWithHigherValueThan1B.length shouldBe 2
+	}
+}
