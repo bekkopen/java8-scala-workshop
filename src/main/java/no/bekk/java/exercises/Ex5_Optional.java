3d6c3e9bf32c18c722225cbf9076517124a24195
Frithjof Nerdrum <frithjof.nerdrum@gmail.com>
2014-05-27 00:33:03 +0200

    Added exercises for Optional

---
+++ src/main/java/no/bekk/java/exercises/Ex5_Optional.java
@@ -0,0 +1,19 @@
+package no.bekk.java.exercises;
+
+import no.bekk.java.model.Player;
+import no.bekk.java.model.Team;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+public class Ex5_Optional {
+
+    static Optional<Player> youngestPlayerOlderThan(Team team, LocalDate minDate) {
+        return team.getPlayers().stream()
+                .filter(player -> player.birthDate.isBefore(minDate))
+                .reduce((x, y) -> {
+                    if (x.birthDate.isAfter(y.birthDate)) return x;
+                    else return y;
+                });
+    }
+}
