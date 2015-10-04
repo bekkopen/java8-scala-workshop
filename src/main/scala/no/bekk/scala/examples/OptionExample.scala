aef14143c01ce077d65ff388c64e0f8969fd21f8
Matias Vinjevoll <matiasvinjevoll@gmail.com>
2015-10-04 17:51:47 +0200

    Update Option Scala example

--- src/main/scala/no/bekk/scala/examples/OptionExample.scala
+++ src/main/scala/no/bekk/scala/examples/OptionExample.scala
@@ -3,7 +3,7 @@ package no.bekk.scala.examples
 object OptionExample {
 
 	def getNameOfOwner(house: House) =
-		Option(house).map(_.owner).flatMap(Option(_)).map(_.name).getOrElse("Doesn't exist")
+		Option(house).map(_.owner).flatMap(Option(_)).map(_.name).flatMap(Option(_)).getOrElse("Doesn't exist")
 }
 
 case class Person(name: String)
